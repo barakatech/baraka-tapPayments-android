e24d452988156de8c340d193bb65c1367e21e8bf
Alexandre DUARTE <alexandre.jose.duarte@gmail.com>
2024-01-24 16:34:06 +0100

    nullify card web url prefix on error and success

--- tapcardformkit/src/main/java/company/tap/tapcardformkit/open/web_wrapper/TapCardKit.kt
+++ tapcardformkit/src/main/java/company/tap/tapcardformkit/open/web_wrapper/TapCardKit.kt
@@ -130,7 +130,7 @@ class TapCardKit : LinearLayout {
         cardCvv: String = "",
         cardHolderName: String =""
     ) {
-        if (cardUrlPrefix == null) {
+        if (cardUrlPrefix == null) { // To avoid duplicated load urls
             cardUrlPrefix = urlWebStarter
             cardPrefillPair = Pair(cardNumber, cardExpiry)
             cardExtraPrefillPair = Pair(cardCvv,cardHolderName)
@@ -278,6 +278,7 @@ class TapCardKit : LinearLayout {
 
                 }
                 if (request?.url.toString().contains(CardFormWebStatus.onError.name)) {
+                    cardUrlPrefix = null // To allow retry if error or success
                     DataConfiguration.getTapCardStatusListener()
                         ?.onError(request?.url?.getQueryParameterFromUri(keyValueName).toString())
                 }
@@ -286,6 +287,7 @@ class TapCardKit : LinearLayout {
 
                 }
                 if (request?.url.toString().contains(CardFormWebStatus.onSuccess.name)) {
+                    cardUrlPrefix = null // To allow retry if error or success
                     DataConfiguration.getTapCardStatusListener()
                         ?.onSuccess(request?.url?.getQueryParameterFromUri(keyValueName).toString())
                 }
